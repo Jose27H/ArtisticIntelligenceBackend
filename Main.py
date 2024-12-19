a4679da9409e1d9307eeed6c768c8e6eea5026ad
ccorbett0116 <100178889+ccorbett0116@users.noreply.github.com>
2024-12-19 14:22:44 -0500

    removed purge db code

--- Main.py
+++ Main.py
@@ -44,11 +44,6 @@ class Image(db.Model):
         self.user_id = user_id
         self.image = image
 
-with app.app_context():
-  db.session.query(Image).delete()
-  db.session.commit()
-
-
 @app.route('/login', methods=['POST'])
 def login():
     print("login init")
